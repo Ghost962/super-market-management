5e04d0825db2ce57ba80d09570ce7a2e0056dfc5
Hisham Nasser <hashooomcs7@gmail.com>
2021-11-19 18:03:14 +0400

    Design

---
+++ UseCases/ViewCategoryUseCase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using CoreBusiness;
+using UseCases.DataStorePluginInterfaces;
+using UseCases.UseCaseInterfaces;
+
+namespace UseCases
+{
+    public class ViewCategoriesUseCase: IViewCategoriesUseCase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ViewCategoriesUseCase(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IEnumerable<Category> Execute()
+        {
+            return _categoryRepository.GetCategories();
+        }
+    }
+}
